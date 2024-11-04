a25b2d458d8981dc1decb3ca7d62696bea90ff3c
AliFathi1325 <Ali.Fathi1325@gmail.com>
2024-11-05 00:17:07 +0330

    Upload the program logic

---
+++ main.py
@@ -0,0 +1,255 @@
+import MetaTrader5 as mt5
+from datetime import datetime, timedelta
+import time
+from models import init_db, save_buy, save_sell, update_ticket, update_result, update_ticket_out
+import pytz
+import asyncio
+from telegram import Bot
+from dotenv import load_dotenv
+import os
+
+
+class AutoTrading():
+    def __init__(self, ACCOUNT=int, PASSWORD=str, SERVER=str, symbol=str, risk=float,) -> None:
+        self.symbol = symbol
+        self.risk = risk
+        if not mt5.initialize():
+            print("MetaTrader5 initialization failed")
+            mt5.shutdown()
+        if not mt5.login(ACCOUNT, PASSWORD, SERVER):
+            print("Login failed")
+            mt5.shutdown()
+
+    def get_last_candle_open_time(self):
+        rates = mt5.copy_rates_from_pos(self.symbol, mt5.TIMEFRAME_M1, 0, 1)
+        if rates is not None and len(rates) > 0:
+            return datetime.fromtimestamp(rates[0][0])
+        return None
+
+    def position_size(self, sl_price, entry_price):
+        symbol_info = mt5.symbol_info(self.symbol)
+        if symbol_info is None:
+            print(f"Failed to get {self.symbol} info")
+            return 0.01
+        contract_size = symbol_info.trade_contract_size
+        point = symbol_info.point
+        sl_distance = abs(entry_price - sl_price)
+        if sl_distance == 0:
+            return 0.01
+        if self.symbol == "XAUUSD":
+            volume = (self.risk/100) / sl_distance
+            volume = round(volume, 2)
+            volume = min(max(volume, 0.01), 10.0)
+        else:
+            pip_value = (contract_size * point)
+            if self.symbol.endswith('USD'):
+                pip_value = pip_value
+            elif self.symbol.startswith('USD'):
+                pip_value = pip_value / entry_price
+            else:
+                pip_value = pip_value
+            volume = 10 / (sl_distance / point * pip_value)
+            volume = round(volume, 2)
+            volume = min(max(volume, 0.01), 10.0)
+        return volume
+
+    def get_candle(self):
+        rates = mt5.copy_rates_from_pos(self.symbol, mt5.TIMEFRAME_M1, 2, 9)
+        if rates is None or len(rates) > 9:
+            return None
+        lis = []
+        for rate in rates:
+            candle = rate[4] - rate[1]
+
+            rates_dict = {
+                'open': rate[1],
+                'close': rate[4],
+                'candle': candle
+            }
+            lis.append(rates_dict)
+        buy_values = [entry['candle'] for entry in lis if entry['candle'] > 0]
+        sell_values = [entry['candle'] for entry in lis if entry['candle'] < 0]
+        min_buy = min(buy_values) if buy_values else 0
+        sum_buy = sum(buy_values) if buy_values else 0
+        len_buy = len(buy_values) if buy_values else 1
+        max_buy = max(buy_values) if buy_values else 0
+        min_sell = min(sell_values) if sell_values else 0
+        sum_sell = sum(sell_values) if sell_values else 0
+        len_sell = len(sell_values) if sell_values else 1
+        max_sell = max(sell_values) if sell_values else 0
+
+        buy_values_dict = {
+            'buy_values_one': min_buy,
+            'buy_values_two': ((sum_buy / len_buy) + max_buy) / 2,
+            'buy_values_three': sum_buy / len_buy,
+            'buy_values_four': ((sum_buy / len_buy) + max_buy) / 2,
+            'buy_values_five': max_buy,
+        }
+        sell_values_dict = {
+            'sell_values_one':  min_sell,
+            'sell_values_two': ((sum_sell / len_sell) + max_sell) / 2,
+            'sell_values_three': sum_sell / len_sell,
+            'sell_values_four': ((sum_sell / len_sell) + max_sell) / 2,
+            'sell_values_five': max_sell,
+        }
+        candle_now = mt5.copy_rates_from_pos(self.symbol, mt5.TIMEFRAME_M1, 1, 1)
+        if candle_now is None or len(candle_now) == 0:
+            return None
+        candle_now = candle_now[0][4] - candle_now[0][1]
+        if candle_now == 0:
+            return 0    
+        if buy_values_dict['buy_values_five'] < candle_now:
+            return 6
+        elif buy_values_dict['buy_values_four'] < candle_now <= buy_values_dict['buy_values_five']:
+            return 5
+        elif buy_values_dict['buy_values_three'] < candle_now <= buy_values_dict['buy_values_four']:
+            return 4
+        elif buy_values_dict['buy_values_two'] < candle_now <= buy_values_dict['buy_values_three']:
+            return 3
+        elif buy_values_dict['buy_values_one'] < candle_now <= buy_values_dict['buy_values_two']:
+            return 2
+        elif 0 < candle_now <= buy_values_dict['buy_values_one']:
+            return 1
+        elif sell_values_dict['sell_values_five'] < candle_now < 0:
+            return -1
+        elif sell_values_dict['sell_values_four'] < candle_now <= sell_values_dict['sell_values_five']:
+            return -2
+        elif sell_values_dict['sell_values_three'] < candle_now <= sell_values_dict['sell_values_four']:
+            return -3
+        elif sell_values_dict['sell_values_two'] < candle_now <= sell_values_dict['sell_values_three']:
+            return -4
+        elif sell_values_dict['sell_values_one'] < candle_now <= sell_values_dict['sell_values_two']:
+            return -5
+        elif candle_now <= sell_values_dict['sell_values_one']:
+            return -6
+
+    def get_candle_power(self):
+        candle_1 = mt5.copy_rates_from_pos(self.symbol, mt5.TIMEFRAME_M1, 1, 1)
+        candle_2 = mt5.copy_rates_from_pos(self.symbol, mt5.TIMEFRAME_M1, 2, 1)
+        trend = candle_1[0][4] - candle_1[0][1]
+        power = candle_1[0][4] - candle_2[0][2] if trend > 0 else candle_2[0][3] - candle_1[0][4] 
+        power_candle = 0 if power < 0 else 1
+        return power_candle
+
+    def get_moving_average(self, period=int):
+        rates = mt5.copy_rates_from_pos(self.symbol, mt5.TIMEFRAME_M1, 1, period)
+        if rates is None or len(rates) < period:
+            print("Failed to get rates.")
+            return None
+        list_price = []
+        for rate in rates:
+            list_price.append(rate['close'])
+        moving_average = sum(list_price)/len(list_price)
+
+        candle_now = mt5.copy_rates_from_pos(self.symbol, mt5.TIMEFRAME_M1, 0, 1)
+        if candle_now is None or len(candle_now) == 0:
+            print("Failed to get current candle")
+            return None
+        pip_moving = candle_now[0][1] - moving_average
+        return pip_moving
+
+    def open_position(self, order_type, risk_reward=1.0, comment="NULL"):
+        order = mt5.ORDER_TYPE_BUY if order_type == "BUY" else mt5.ORDER_TYPE_SELL
+        enter_price = mt5.symbol_info_tick(self.symbol).ask if order == mt5.ORDER_TYPE_BUY else mt5.symbol_info_tick(self.symbol).bid
+        enter_candle = mt5.copy_rates_from_pos(self.symbol, mt5.TIMEFRAME_M1, 1, 1)
+        spread = (mt5.symbol_info_tick(self.symbol).ask - mt5.symbol_info_tick(self.symbol).bid)*1.5
+        sl = round(enter_candle[0][3]-(spread/3) if order == mt5.ORDER_TYPE_BUY else enter_candle[0][2]+spread, 2)
+        tp = round(((enter_price-sl)*risk_reward)+enter_price if order == mt5.ORDER_TYPE_BUY else enter_price-((sl-enter_price)*risk_reward), 2)
+        volume = self.position_size(sl, enter_price)
+        request = {
+        "action": mt5.TRADE_ACTION_DEAL,
+        "symbol": self.symbol,
+        "volume": volume,
+        "type": order,
+        "price": enter_price,
+        "sl": sl,
+        "tp": tp,
+        "deviation": 20,
+        "magic": 234000,
+        "comment": comment,
+        "type_time": mt5.ORDER_TIME_GTC,
+        "type_filling": mt5.ORDER_FILLING_IOC,
+        }
+        result = mt5.order_send(request)
+        if result.retcode != mt5.TRADE_RETCODE_DONE:
+            print("Error", result.comment)
+            return None
+        ticket = result.order
+        return ticket
+
+    def update_tiket_result(self):
+        utc_now = datetime.now(pytz.utc)
+        tz = pytz.timezone('Etc/GMT-2')
+        end_date = utc_now.astimezone(tz) + timedelta(days=1)
+        start_date = end_date - timedelta(days=2)
+        deals = mt5.history_deals_get(start_date, end_date)
+        for deal_in in deals:
+            if deal_in.profit == 0 and deal_in.entry == mt5.DEAL_ENTRY_IN:
+                ticket_in = deal_in.order
+                try:
+                    ticket_out = mt5.history_orders_get(position = ticket_in)[1][0]
+                except:
+                    pass
+                for deal_out in deals:
+                    if deal_out.profit != 0 and deal_out.entry == mt5.DEAL_ENTRY_OUT:
+                        if ticket_out:
+                            if deal_out.order == ticket_out:
+                                profit = deal_out.profit
+                                profit = 1 if profit > 0 else 0
+                                update_result(profit, ticket_in)
+                                update_ticket_out(ticket_out, ticket_in)
+                                ticket_in = None
+                                ticket_out = None
+                                profit = None
+
+    def int_moving(self, moving5, moving15, moving60):
+        numbers = [0, moving5, moving15, moving60]
+        sorted_numbers = sorted(numbers)  
+        scores = {value: rank for rank, value in enumerate(sorted_numbers)}
+        return [scores[0], scores[moving5], scores[moving15], scores[moving60]]
+    
+async def send_telegram(text):
+    load_dotenv()
+    bot = Bot(token=os.getenv("BOT_TOKEN"))
+    await bot.send_message(chat_id=os.getenv("CHANEL"), text=text)  
+ 
+def run_bot(ACCOUNT, PASSWORD, SERVER, symbol, risk, risk_reward, run):
+    trade = AutoTrading(ACCOUNT, PASSWORD, SERVER, symbol, risk)
+    init_db()
+    asyncio.run(send_telegram('Connecting to MetaTrader5'))
+    last_candle_open_time = trade.get_last_candle_open_time()
+    start_time = datetime.strptime("12:30", "%H:%M").time()
+    end_time = datetime.strptime("23:30", "%H:%M").time()
+    while True:
+        if run == 1 and start_time <= last_candle_open_time.time() <= end_time:
+            time.sleep(1)
+            current_open_time = trade.get_last_candle_open_time()
+            candle = trade.get_candle()
+            power = trade.get_candle_power()
+            moving5 = trade.get_moving_average(5)
+            moving15 = trade.get_moving_average(15)
+            moving60 = trade.get_moving_average(60)
+            int_moving = trade.int_moving(moving5, moving15, moving60)
+            if current_open_time != last_candle_open_time:
+                if candle != None:
+                    if candle > 0 and moving15 > 0:
+                        teda_id = save_buy(candle, power, int(int_moving[0]), int(int_moving[1]), int(int_moving[2]), int(int_moving[3]))
+                        comment = f"Buy-{str(teda_id)}"
+                        ticket_in = trade.open_position("BUY", risk_reward, comment)
+                        update_ticket(teda_id, comment, ticket_in)
+                        asyncio.run(send_telegram(f"{comment} order is opened"))
+                    elif candle < 0 and moving15 < 0:
+                        teda_id = save_sell(abs(candle), power, int(int_moving[0]), int(int_moving[1]), int(int_moving[2]), int(int_moving[3]))
+                        comment = f"Sell-{str(teda_id)}"
+                        ticket_in = trade.open_position("SELL", risk_reward, comment)
+                        update_ticket(teda_id, comment, ticket_in)
+                        asyncio.run(send_telegram(f"{comment} order is opened"))
+                    else:
+                        trade.update_tiket_result()
+                last_candle_open_time = current_open_time
+        else:
+            last_candle_open_time = trade.get_last_candle_open_time()
+            trade.update_tiket_result()
+
+if __name__ == "__main__":
+    run_bot()
---
+++ models.py
@@ -0,0 +1,144 @@
+import sqlite3
+
+def init_db():
+    conn = sqlite3.connect('swing.db')
+    cursor = conn.cursor()
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS buy (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            Candle INTEGER NOT NULL,
+            Power INTEGER NOT NULL,
+            price INTEGER NOT NULL,
+            MovingAverage5 INTEGER NOT NULL,
+            MovingAverage15 INTEGER NOT NULL,
+            MovingAverage60 INTEGER NOT NULL,
+            ticket_in INTEGER NULL,
+            ticket_out INTEGER NULL,
+            comment TEXT NULL,
+            result INTEGER NULL
+        )
+    ''')
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS sell (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            Candle INTEGER NOT NULL,
+            Power INTEGER NOT NULL,
+            price INTEGER NOT NULL,
+            MovingAverage5 INTEGER NOT NULL,
+            MovingAverage15 INTEGER NOT NULL,
+            MovingAverage60 INTEGER NOT NULL,
+            ticket_in INTEGER NULL,
+            ticket_out INTEGER NULL,
+            comment TEXT NULL,
+            result INTEGER NULL
+        )
+    ''')
+    cursor.execute('''
+            CREATE TABLE IF NOT EXISTS users (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                username TEXT UNIQUE NOT NULL,
+                email TEXT UNIQUE NOT NULL,
+                password TEXT NOT NULL
+        )
+    ''')
+    conn.commit()
+    conn.close()
+
+def save_user(username, email, password):
+    conn = sqlite3.connect('swing.db')
+    cursor = conn.cursor()
+    cursor.execute('''
+        INSERT INTO users (username, email, password)
+        VALUES (?, ?, ?)
+    ''', (username, email, password))
+    conn.commit()
+    conn.close()
+    
+def login_user(username, password):
+    conn = sqlite3.connect('swing.db')
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM users WHERE username = ? AND password = ?", (username, password))
+    user = cursor.fetchone()
+    conn.close()
+    return user
+
+def save_buy(Candle, Power, price, MovingAverage5, MovingAverage15, MovingAverage60):
+    conn = sqlite3.connect('swing.db')
+    cursor = conn.cursor()
+    cursor.execute('''
+        INSERT INTO buy (Candle, Power, price, MovingAverage5, MovingAverage15, MovingAverage60)
+        VALUES (?, ?, ?, ?, ?, ?)
+    ''', (Candle, Power, price, MovingAverage5, MovingAverage15, MovingAverage60))
+    transaction_id = cursor.lastrowid
+    conn.commit()
+    conn.close()
+    return transaction_id
+
+def save_sell(Candle, Power, price, MovingAverage5, MovingAverage15, MovingAverage60):
+    conn = sqlite3.connect('swing.db')
+    cursor = conn.cursor()
+    cursor.execute('''
+        INSERT INTO sell (Candle, Power, price, MovingAverage5, MovingAverage15, MovingAverage60)
+        VALUES (?, ?, ?, ?, ?, ?)
+    ''', (Candle, Power, price, MovingAverage5, MovingAverage15, MovingAverage60))
+    transaction_id = cursor.lastrowid
+    conn.commit()
+    conn.close()
+    return transaction_id
+
+def update_ticket(transaction_id, comment, ticket_in):
+    conn = sqlite3.connect('swing.db')
+    cursor = conn.cursor()
+    cursor.execute(''' 
+        UPDATE buy 
+        SET ticket_in = ?, comment = ?
+        WHERE id = ? 
+    ''', (ticket_in, comment, transaction_id))
+    cursor.execute(''' 
+        UPDATE sell 
+        SET ticket_in = ?, comment = ?
+        WHERE id = ? 
+    ''', (ticket_in, comment, transaction_id))
+    conn.commit()
+    conn.close()
+
+def update_result(result, ticket_in):
+    conn = sqlite3.connect('swing.db')
+    cursor = conn.cursor()
+    cursor.execute(''' 
+        UPDATE buy 
+        SET result = ?
+        WHERE ticket_in = ? 
+    ''', (result, ticket_in))
+    cursor.execute(''' 
+        UPDATE sell 
+        SET result = ?
+        WHERE ticket_in = ? 
+    ''', (result, ticket_in))
+    conn.commit()
+    conn.close()
+
+def update_ticket_out(ticket_out, ticket_in):
+    conn = sqlite3.connect('swing.db')
+    cursor = conn.cursor()
+    cursor.execute(''' 
+        UPDATE buy 
+        SET ticket_out = ?
+        WHERE ticket_in = ? 
+    ''', (ticket_out, ticket_in))
+    cursor.execute(''' 
+        UPDATE sell 
+        SET ticket_out = ?
+        WHERE ticket_in = ? 
+    ''', (ticket_out, ticket_in))
+    conn.commit()
+    conn.close()
+
+
+def get_data_from_db(table_name):
+    conn = sqlite3.connect('swing.db')
+    cursor = conn.cursor()
+    cursor.execute(f"SELECT id, Candle, Power, Price, MovingAverage5, MovingAverage15, MovingAverage60, Result FROM {table_name}")
+    data = cursor.fetchall()
+    conn.close()
+    return data
